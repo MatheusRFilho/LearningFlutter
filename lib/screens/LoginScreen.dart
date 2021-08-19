59b8a37d7210df4d9bd0155d22709e5ecaa87129
matheusrfilho <matheus.rodrigo@in8.com.br>
2021-08-19 09:44:33 -0300

    first commit

---
+++ lib/screens/LoginScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('test'),
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
